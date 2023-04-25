855aaf558d4012f3b0ec3367530291fbae41f662
albertobKlick <abonora@klick.com>
2023-04-25 17:25:40 -0400

    fixed ISI

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,35 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+//ISI Stuff
+const isi = document.querySelectorAll('.isi')[0];
+const fixedISI = document.createElement('div');
+const body = document.getElementsByTagName('body')[0];
+fixedISI.innerHTML = isi.innerHTML;
+fixedISI.classList.add('fixed-isi');
+isi.parentNode.append(fixedISI);
+
+function toggle(el, classname){
+    el.classList.toggle(classname);
+}
+function handleIntersection(entries){
+    entries.map((entry) => {
+        if(entry.isIntersecting){
+            fixedISI.classList.add('hide-fixed-isi');
+        }else{
+            fixedISI.classList.remove('hide-fixed-isi');
+        }
+    },{
+        threshold: 1
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersection);
+observer.observe(isi);
+fixedISI.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggle(body,'fixed');
+    toggle(fixedISI,'expanded');
+    fixedISI.scrollTop = 0;
+});
