a661fe999f1272ef39cecd9de8cde631aee48557
edemiraj-klick <edemiraj@klick.com>
2023-04-25 18:41:31 +0200

    styles update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,11 +21,13 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
  */
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
+  const h5 = main.querySelector('h5');
   const picture = main.querySelector('picture');
+  const a = main.querySelector('a');
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    section.append(buildBlock('hero', { elems: [picture, h1, h5, a] }));
     main.prepend(section);
   }
 }
